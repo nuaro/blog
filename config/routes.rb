500bca531f0d14f2307181d322dfb7cb36c3ebcb
Hernan Zambrano <hernan.antonio.zambrano@gmail.com>
2015-10-04 01:04:52 +0000

    new contreller and model for articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+ 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+   resources :articles
 
   # Example resource route with options:
   #   resources :products do
